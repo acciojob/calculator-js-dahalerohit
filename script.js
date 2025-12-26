429bbc80da8ddf36f251a82e8caac5131717abbe
Rohit Dahale <151941395+dahalerohit@users.noreply.github.com>
2025-12-27 01:53:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,40 @@
-//your JS code here. If required.
+//your JS code here. If required
+
+const buttons = document.querySelectorAll(".btn");
+const display = document.querySelector("#display");
+
+buttons.forEach(btn => {
+  btn.addEventListener("click", () => {
+    const id = btn.id;
+    const value = btn.innerText;
+
+    if (id === "C") {
+      display.innerText = "";
+    }
+    else if (id === "back") {
+      display.innerText = display.innerText.slice(0, -1);
+    }
+  
+    else if (id === "equal") {
+  try {
+    const result = eval(display.innerText);
+
+    // If result is Infinity or NaN, show Error
+    if (result === Infinity || result === -Infinity || isNaN(result)) {
+      display.innerText = "Error";
+    } else {
+      display.innerText = result;
+    }
+
+  } catch {
+    display.innerText = "Error";
+  }
+}
+
+
+    else {
+      display.innerText += value;
+    }
+  });
+});
+
